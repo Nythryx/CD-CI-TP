e99547de3ef17a0f99baf3563875701231237ed2
Arnaud <95078809+Nythryx@users.noreply.github.com>
2025-06-27 10:07:36 +0200

    Update TestSimpleMath.py

--- TestSimpleMath.py
+++ TestSimpleMath.py
@@ -5,5 +5,9 @@ class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(SimpleMath.addition(2, 3), 5)
 
+     def test_soustraction(self):
+        self.assertEqual(SimpleMath.soustraction(5, 2), 3)
+
+
 if __name__ == '__main__':
     unittest.main()
